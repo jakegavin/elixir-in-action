f0beaaaf5ffb14deb38923e4e8cc196f30899bee
Jake Gavin <jakegavin@gmail.com>
2016-09-08 17:21:46 -0700

    Ch 6: Use GenServer for TodoServer

--- jakegavin/ch06/180/todo_server.ex
+++ jakegavin/ch06/180/todo_server.ex
@@ -1,26 +1,28 @@
 defmodule TodoServer do
+  use GenServer
+
   def start do
-    ServerProcess.start(TodoServer)
+    GenServer.start(TodoServer, nil)
   end
 
   def add_entry(todo_server, new_entry) do
-    ServerProcess.cast(todo_server, {:add_entry, new_entry})
+    GenServer.cast(todo_server, {:add_entry, new_entry})
   end
 
   def entries(todo_server, date) do
-    ServerProcess.call(todo_server, {:entries, date})
+    GenServer.call(todo_server, {:entries, date})
   end
 
-  def init do
-    TodoList.new
+  def init(_) do
+    {:ok, TodoList.new}
   end
 
   def handle_cast({:add_entry, new_entry}, todo_list) do
-    TodoList.add_entry(todo_list, new_entry)
+    {:noreply, TodoList.add_entry(todo_list, new_entry)}
   end
 
-  def handle_call({:entries, date}, todo_list) do
-    {TodoList.entries(todo_list, date), todo_list}
+  def handle_call({:entries, date}, _, todo_list) do
+    {:reply, TodoList.entries(todo_list, date), todo_list}
   end
 end
 
@@ -87,37 +89,5 @@ defmodule TodoList do
     entry_id
   ) do
     %TodoList{todo_list | entries: Map.delete(entries, entry_id)}
-  end
-end
-
-defmodule ServerProcess do
-  def start(callback_module) do
-    spawn(fn ->
-      initial_state = callback_module.init
-      loop(callback_module, initial_state)
-    end)
-  end
-
-  def call(server_pid, request) do
-    send(server_pid, {:call, request, self})
-    receive do
-      {:response, response} -> response
-    end
-  end
-
-  def cast(server_pid, request) do
-    send(server_pid, {:cast, request})
-  end
-
-  defp loop(callback_module, current_state) do
-    receive do
-      {:call, request, caller} ->
-        {response, new_state} = callback_module.handle_call(request, current_state)
-        send(caller, {:response, response})
-        loop(callback_module, new_state)
-      {:cast, request} ->
-        new_state = callback_module.handle_cast(request, current_state)
-        loop(callback_module, new_state)
-    end
   end
 end
